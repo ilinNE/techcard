30c522c42fae38bb13d70672a720cbd4a48f5e44
Yulia Ovchinnikova <yulia.ovchinnikova.92@gmail.com>
2022-07-29 12:23:34 +0300

    Fix: clean up Main.tsx, delete get/post requests

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,51 +1,17 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import "./Main.scss";
-import * as Api from "../../utils/Api";
 import Header from "../Header/Header";
 import Greeting from "../../components/Main/Greeting/Greeting";
 
+interface MainProps {
+  loggedIn: boolean;
+}
 
-const Main: FC = () => {
-  function handleGet() {
-    Api.getTest()
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  function handlePost() {
-    Api.postTest("Тестовая строка")
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+const Main: FC<MainProps> = ({ loggedIn }) => {
 
   return (
     <div>
-      <h1>Главная страница</h1>
-      <button className="button" onClick={handleGet}>
-        Get
-      </button>
-      <button className="button" onClick={handlePost}>
-        Post
-      </button>
-      <p id="test1" className="light">
-        Montserrat-Light
-      </p>
-      <p className="Medium">Montserrat-Medium</p>
-      <p className="SemiBold">Montserrat-SemiBold</p>
-      <br />
-      <p id="test2" className="light">
-        Монсеррат-Светлый
-      </p>
-      <p className="Medium">Монсеррат-Средний</p>
-      <p className="SemiBold">Монсеррат-Полужирный</p>
+      <Header loggedIn={loggedIn} />
       <Greeting />
     </div>
   );
