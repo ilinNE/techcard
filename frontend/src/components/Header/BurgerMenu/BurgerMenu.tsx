13ca9dfd5fd7424816ee87a14962ba33bfc56db4
TarStaBor <TarStaBor@yandex.ru>
2022-08-11 13:57:45 +0300

    fix: update burgerMenu

--- frontend/src/components/Header/BurgerMenu/BurgerMenu.tsx
+++ frontend/src/components/Header/BurgerMenu/BurgerMenu.tsx
@@ -1,5 +1,4 @@
-import { FC, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { FC } from "react";
 import Avatar from "../../Avatar/Avatar";
 import Nav from "../Nav/Nav";
 import "./BurgerMenu.scss";
@@ -12,9 +11,11 @@ interface BurgerMenuProps {
 const BurgerMenu: FC<BurgerMenuProps> = ({ handleCloseBurgerMenu, isBurgerOpen }) => {
   return (
     <section className={`burgerMenu ${isBurgerOpen && "burgerMenu_opened"}`}>
-      <div className="burgerMenu__container">
-        <Avatar handleCloseBurgerMenu={handleCloseBurgerMenu} isBurgerOpen={isBurgerOpen} />
-        <Nav handleCloseBurgerMenu={handleCloseBurgerMenu} isBurgerOpen={isBurgerOpen} />
+      <div className={`burgerMenu__menu  ${isBurgerOpen && "burgerMenu__menu_opened"}`}>
+        <div className="burgerMenu__container">
+          <Avatar handleCloseBurgerMenu={handleCloseBurgerMenu} isBurgerOpen={isBurgerOpen} />
+          <Nav handleCloseBurgerMenu={handleCloseBurgerMenu} isBurgerOpen={isBurgerOpen} />
+        </div>
       </div>
     </section>
   );
