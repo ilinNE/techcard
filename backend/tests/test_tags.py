53d8c3bcde25e1eec9ca05667c2aae2331543d24
Nikita Ilin <ilinWall@yandex.ru>
2022-08-29 21:10:58 +0300

    test: tag tests done

--- backend/tests/test_tags.py
+++ backend/tests/test_tags.py
@@ -1,5 +1,5 @@
 import pytest
-from cards.models import Tag, Product
+from cards.models import Tag
 
 
 class TestTag:
@@ -10,7 +10,7 @@ class TestTag:
     }
 
     @pytest.mark.django_db()
-    def test_create__invalid_data(self, user_client):
+    def test_tag_create__invalid_data(self, user_client):
         tag_count = Tag.objects.count()
         response = user_client.post(self.url)
         assert response.status_code == 400, (
@@ -34,7 +34,7 @@ class TestTag:
         )
 
     @pytest.mark.django_db()
-    def test_create__unauthorized(self, client):
+    def test_tag_create__unauthorized(self, client):
         tag_count = Tag.objects.count()
         response = client.post(self.url, data=self.valid_data)
         assert response.status_code == 401, (
@@ -44,8 +44,8 @@ class TestTag:
             "Неавторизованный пользователь не может создать тэг"
         )
 
-    @pytest.mark.django_db()
-    def test_create__valid_data(self, user_client):
+    @pytest.mark.django_db
+    def test_tag_create__valid_data(self, user_client):
         tag_count = Tag.objects.count()
         response = user_client.post(self.url, data=self.valid_data)
         assert response.status_code == 201, (
@@ -55,19 +55,86 @@ class TestTag:
             "При валидном запросе, тэг должен быть добавлен в БД"
         )
 
-    @pytest.mark.django_db()
-    def test_tag_1(self,user, user_client, tag_1):
-        assert tag_1.owner == user, "Error"
+    @pytest.mark.django_db
+    def test_tag_get_list(self, user_client, tag_1, tag_2):
+        response = user_client.get(self.url)
+        assert response.status_code == 200, (
+            "Запрос списка тэгов работает некорректно.",
+            f"Код ответа: {response.status_code}"
+        )
+        assert len(response.json()) <= 2, (
+            "Список тэгов содержит лишние элементы"
+        )
+        assert len(response.json()) >= 2, (
+            "Список тэгов не полный"
+        )
     
-    @pytest.mark.django_db()
-    def test_create_product(self,user, user_client, product_1, product_2):
-        assert product_1.tags.all() != "sdfs", "Error"
-        assert product_2.owner == user, "Error"
+    @pytest.mark.django_db
+    def test_tag_retrieve(self, user_client, tag_1):
+        url = self.url + str(tag_1.id) + '/'
+        response = user_client.get(url)
+        assert response.status_code == 200, (
+            f"Запрос отдельного тэга работает некорректно.",
+            f"Код ответа: {response.status_code}"
+        )
+        assert response.json().get('id') == tag_1.id, (
+            "Получены данныые не запрашиваемого обьекта"
+        )
+        expected_fields = ['id', 'name', 'color']
+        for field in expected_fields:
+            assert field in response.json().keys(), (
+                f"Поле {field}, отсутствует в ответе"
+            )
 
-    @pytest.mark.django_db()
-    def test_techcard(self,user, semifabricate):
-        product_list = Product.objects.all()
-        assert semifabricate.name in product_list, 'error'
-        
+    @pytest.mark.django_db
+    def test_tag_update(self, user_client, tag_1):
+        url = self.url + str(tag_1.id) + '/'
+        update_data = { "name": "green", "color": "#00ff00"}
+        response = user_client.put(url, data=update_data)
+        assert response.status_code == 200, (
+            "Запрос на обновление данных тэга работает некоректно"
+        )
+        for field, value in update_data.items():
+            assert response.json().get(field) == value, (
+                f"Данные не были обновлены. Поле {field}"
+            )
+    
+    @pytest.mark.django_db
+    def test_tag_delete(self, user_client, tag_1):
+        tag_count = Tag.objects.count()
+        url = self.url + str(tag_1.id) + '/'
+        response = user_client.delete(url)
+        assert response.status_code == 204, (
+            "Удаление тэга работатет некорректно"
+        )
+        assert Tag.objects.count() == tag_count - 1, (
+            "Тэг не был удален"
+        )
+
+    @pytest.mark.django_db
+    def test_tag__another_user_access(self, another_user_client, tag_1):
+        tag_count = Tag.objects.count()
+        url = self.url + str(tag_1.id) + '/'
+        response = another_user_client.get(url)
+        assert response.status_code == 404, (
+            "Пользователь не может получить данные о чужом тэге"
+        )
+        update_data = { "name": "green", "color": "#00ff00"}
+        response = another_user_client.put(url, data=update_data)
+        assert response.status_code == 404, (
+            "Пользователь не может изменить чужой тэг"
+        )
+        response = another_user_client.delete(url)
+        assert response.status_code == 404, (
+            "Пользователь не может удалить чужой тэг"
+        )
+        assert  tag_count == Tag.objects.count(), (
+            "Пользователь не может удалить чужой тэг"
+        )
+
+             
+
+
+
 
     
