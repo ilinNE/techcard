06bca7d51ecfaa1f049c6d39998abe3ee7e59ae1
Yulia Ovchinnikova <yulia.ovchinnikova.92@gmail.com>
2022-10-21 17:03:19 +0200

    Add styling for EmptyContent component

--- frontend/src/components/EmptyContent/EmptyContent.tsx
+++ frontend/src/components/EmptyContent/EmptyContent.tsx
@@ -1,8 +1,10 @@
+import "./EmptyContent.scss";
+
 export const EmptyContent = () => {
   return (
     <>
-      <p>Пока что пусто</p>
-      <button>Добавить</button>
+      <p className="empty-title">Пока что пусто</p>
+      <button className="add-button">Добавить</button>
     </>
   );
 };
